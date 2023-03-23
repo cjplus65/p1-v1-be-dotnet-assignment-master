85e3f942ae2f2adbf62bc93a7dc465ca98058811
Chirath Jayawardena <cjplus65@users.noreply.github.com>
2023-03-24 01:20:07 +0530

    Delete API directory

--- API/ApiResponses/FlightResponse.cs
+++
@@ -1,5 +0,0 @@
-using System;
-
-namespace API.ApiResponses;
-
-public record FlightResponse(string DepartureAirportCode, string ArrivalAirportCode, DateTimeOffset Departure, DateTimeOffset Arrival, decimal PriceFrom);
--- API/Infrastructure/ISeeder.cs
+++
@@ -1,7 +0,0 @@
-namespace API.Infrastructure
-{
-    public interface ISeeder
-    {
-        void Seed();
-    }
-}
