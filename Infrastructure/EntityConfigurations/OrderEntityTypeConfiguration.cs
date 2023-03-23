e982d9a118560f3b11db5e1418c03e3ed3952ddc
Chirath Jayawardena <cjplus65@users.noreply.github.com>
2023-03-24 01:25:17 +0530

    Add files via upload

---
+++ Infrastructure/EntityConfigurations/OrderEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Aggregates.OrderAggregate;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityConfigurations
+{
+    internal class OrderEntityTypeConfiguration : BaseEntityTypeConfiguration<Order>
+    {
+        public override void Configure(EntityTypeBuilder<Order> builder)
+        {
+            base.Configure(builder);
+            builder.Property(i => i.PassengerEmail).IsRequired();
+            builder.Property(i => i.NumberOfSeats).IsRequired();
+            builder.HasOne(x=>x.Flight);
+            builder.HasOne(x => x.FlightRate);
+        }
+    }
+}
